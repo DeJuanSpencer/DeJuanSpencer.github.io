e5806ca9f869770275d682cf0e0f32a8fb57c4c6
DeJuan Spencer <dejuanspencer@gmail.com>
2025-04-15 17:48:32 -0700

    Move resume

--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
           Contact Me
         </a>
         <a
-          href="../../../public/DeJuan Spencer Resume.docx"
+          href="../DeJuan Spencer Resume.docx"
           className="text-gray-600 underline hover:text-blue-600 text-sm pt-2 md:pt-3 block"
           download
         >
