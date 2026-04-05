5c38314d9dac22eee6853e47c37d0ba87f11f3f1
DeJuan Spencer <deJuanspencer@gmail.com>
2026-04-04 18:32:24 -0700

    edit prompt engine header

--- frontend/src/app/prompt-engine/page.tsx
+++ frontend/src/app/prompt-engine/page.tsx
@@ -1,11 +1,5 @@
-import Header from "../components/Header";
 import PromptEngine from "../components/PromptEngine";
 
 export default function PromptEnginePage() {
-  return (
-    <>
-      <Header />
-      <PromptEngine />
-    </>
-  );
+  return <PromptEngine />;
 }
